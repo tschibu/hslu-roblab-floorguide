834ab8d072457b857ddc70a322cf99c0c798f5f8
Serge <serge.hauri@stud.hslu.ch>
2018-10-04 20:15:59 +0200

    TakePicture working

--- 3dCamera.py
+++ 3dCamera.py
@@ -2,7 +2,20 @@ from pynaoqi_mate import Robot
 from configuration import PepperConfiguration
 import qi
 
-virtualRobotConfig = PepperConfiguration("Porter")
-myRobot = Robot(virtualRobotConfig)
+#virtualRobotConfig = PepperConfiguration("Porter")
+#myRobot = Robot(virtualRobotConfig)
 
 
+class TakePictureExample():
+    def __init__(self):
+        self.config = PepperConfiguration("Porter")
+        robot = Robot(self.config)
+        self.camera = robot.ALPhotoCapture
+
+    def takePicture(self):
+        remote_folder_path ="/home/nao/recordings/cameras/"
+        file_name = "myPicture.jpg"
+        self.camera.takePicture(remote_folder_path, file_name)
+
+app = TakePictureExample()
+app.takePicture()
