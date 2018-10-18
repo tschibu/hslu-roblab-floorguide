d38e4a76327e4cb9eb7465c2308eb9e1fc24a5fe
Serge <serge.hauri@stud.hslu.ch>
2018-10-18 18:55:47 +0200

    fixed file path

--- cameraDemo.py
+++ cameraDemo.py
@@ -17,7 +17,7 @@ class TakePictureExample():
         remote_folder_path = "/home/nao/recordings/cameras/"
         file_name = "myPicture.jpg"
         self.tts.say("say cheese!")
-        qi.async(self.audio.playFile, "/data/home/nao/recordings/mp3/camera_shutter.mp3")
+        qi.async(self.audio.playFile, "/home/nao/recordings/mp3/camera_shutter.mp3")
         time.sleep(3)
         self.camera.takePicture(remote_folder_path, file_name)
 
