8d668e49d280dfc34678a7f615e53fff745fdc5f
inux <inux.steve@gmail.com>
2018-12-05 14:48:38 +0100

    add room seletion webapp

---
+++ webapp_room_selection/html/js/path.js
@@ -0,0 +1,55 @@
+function drawPosition(position) {
+
+    // get rid of position classes
+    let positionElements = Array.from(document.querySelectorAll('.position'));
+    positionElements.forEach(e =>{
+        e.className = "floor";
+    });
+
+    // add new position class to element
+    var id = position['x'] + '-' + position['y'];
+    var deg = position['deg'];
+    var e = document.getElementById(id);
+    e.className += " position";
+    e.className += " deg-" + deg;
+
+}
+
+function drawPath(path) {
+
+    // get rid of path classes
+    let pathElements = Array.from(document.querySelectorAll('.path'));
+    pathElements.forEach(e => {
+        e.classList.remove("path");
+    });
+
+    path.forEach(node => {
+        var id = node['x'] + '-' + node['y'];
+        var e = document.getElementById(id);
+        e.className += " path";
+    });
+
+}
+
+function updateMap() {
+
+    var requestURL = '/json/position.json';
+    var request = new XMLHttpRequest();
+
+    request.open('GET', requestURL+ ((/\?/).test(requestURL) ? "&" : "?") + (new Date()).getTime());
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function () {
+
+        var jsonObj = request.response;
+
+        var position = jsonObj['position'];
+        var path = jsonObj['path'];
+
+        drawPosition(position);
+        drawPath(path);
+
+    }
+
+}
